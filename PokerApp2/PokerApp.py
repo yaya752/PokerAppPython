5d37f6ed3c2d0da45c44ee238af3a6c6722fee9c
Yaya752 <yassine76440@gmail.com>
2023-04-26 17:05:46 +0100

    beginning of the recap of each game

--- PokerApp2/PokerApp.py
+++ PokerApp2/PokerApp.py
@@ -1,14 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
 import os
-from PIL import Image, ImageDraw
-import io
-from Card import Card
-from table import Table
-from Player import Player
-import base64
-import random
-import json
-from function import Summary_Chips ,Average, Generalities, Summary_Hands
+from function import Summary_Chips ,Average, Generalities, Summary_Hands, Init, Play
 app = Flask(__name__, static_folder='static')
 app.secret_key = "PokerApp"
 
@@ -52,9 +44,11 @@ def Summary():
     first_lines = []
     generalities_list = []
     hand_table = []
+    
     for f in files:
         summary_table.append(Summary_Chips(f,main_player))
         hand_table.append(Summary_Hands(f,main_player) )
+        l =     Play("game17.txt")   
         with open("Game_File\\" + f, "r") as f:
             line = f.readline()
             first_lines.append(line.strip())
@@ -72,9 +66,9 @@ Parameters:
 Returns: 
  - Html file that allow us to choose the phase that we want to play
 Description:
-    phase allow to choose what phase we want to try
+    allow to choose what phase we want to try
 '''
-@app.route('/Phases/<int:index>')
+@app.route('/Play/<int:index>')
 def phase(index):
     files = session['files']
     file_name = files[index]
@@ -89,7 +83,7 @@ Function Name: phase 3
 Parameters:
     
 Returns: 
-    HTML of the firs Phase
+    HTML of the first Phase
 Description:
     Create the first phase
 
@@ -101,119 +95,7 @@ def serve_js():
 
 @app.route('/Phases/phase3', methods=['GET', 'POST'])
 def phase3():
-    file_name = session['file_name']
-    main_player = session['main_player']
-    Table_game = Table([],0,0,main_player,0)
-    Table_game1 = Table([],0,0,main_player,0)
-    pos = 0
-    pos1 = 0
-    with open(file_name, "r") as f:
-        ligne = f.readline()
-        while ligne[:5] != "*** 4":
-            mots = ligne.split()
-            if mots[0] == "Seat":
-                chips_str = mots[3][1:]
-                chips = int(chips_str)
-                player = Player(mots[2],[],[],chips,"",-1,0)
-                Table_game.AppendPlayer(player)  
-            elif mots[1] == "posts":
-                ante = int(mots[4])
-                Table_game.Ante=ante 
-                Table_game.Pot+=ante  
-            elif mots[0] == "Dealt":
-                if mots[2] == main_player:
-                    card1 = Card(mots[3][1],mots[3][2])
-                    card2 = Card(mots[4][0],mots[4][1])
-                    card3 = Card(mots[5][0],mots[5][1])
-                    Table_game.DealtAllCards("Hero",card1)
-                    Table_game.DealtAllCards("Hero",card2)
-                    Table_game.DealtSeenCards("Hero",card3)
-                else:
-                    Table_game.DealtSeenCards(mots[2],Card(mots[3][1],mots[3][2]))
-            elif mots[1] == "brings" or mots[1] == "calls" or mots[1] == "folds" or mots[1] == "raises" or mots[1] == "bets":
-                if (pos != len(Table_game.Players  )):
-                    Table_game.Do(mots[0][:-1],mots[1:],pos)
-                    pos+=1
-                else:
-                    player1 = Table_game.GetPlayer(mots[0][:-1])
-                    player1.Position = pos1
-                    Table_game1.AppendPlayer(Player(player1.Name,player1.AllCards,player1.CardSeen,player1.Chips,mots[1:],pos1,player1.ChipsOnTable))
-                    Table_game1.Actions()
-                    pos1+=1
-            ligne = f.readline()
-        Table_game.SetAnte()
-        Table_game1.SetAnte()
-        Table_game.Sort()
-        #Table_game.display()
-        #Table_game1.display()
-        Table_json = json.dumps(Table_game, default=lambda o: o.__json__())
-        Table1_json = json.dumps(Table_game1, default=lambda o: o.__json__())
-        session['table'] = Table_json
-        session['table1'] = Table1_json
-        print(Table1_json)
-        print(Table_json)
-    return render_template('Phase3.html' , Table_json=Table_json, Table1_json=Table1_json )
-
-@app.route('/phase4', methods=['GET', 'POST'])
-def phase4():
     
-    print(4)
-    return render_template('Phase.html')
-
-@app.route('/phase5', methods=['GET', 'POST'])
-def phase5():
-    
-    print(5)
-    return render_template('Phase.html')
-
-@app.route('/phase6', methods=['GET', 'POST'])
-def phase6():
-    
-    print(6)
-    return render_template('Phase.html')
-
-@app.route('/phase7', methods=['GET', 'POST'])
-def phase7():
-
-    print(7)
-    return render_template('Phase.html')
-@app.route('/Phases/Summary', methods=['GET', 'POST'])
-def phaseS():
-    print("s")
-    return render_template('Phase.html')
-'''
-Function Name:
-
-Parameters:
-
-Returns: 
-
-Description:
-
-'''
-
-@app.route('/Poker', methods=['GET', 'POST'])
-def poker_table():
-    file_name = request.args['file_name']
-    file_name = "Game_File\\" + file_name
-    # Background image
-    width, height = 800, 600
-    im = Image.new('RGB', (width, height), (0, 0, 0))
-    
-    # Ddraw the shape of table
-    draw = ImageDraw.Draw(im)
-    draw.ellipse((width/4, height/4, width*3/4, height*3/4), fill=(0, 255, 0))
-    
-    # Convert  PNG image and encoded it in base64
-    img_bytes = io.BytesIO()
-    im.save(img_bytes, format='PNG')
-    img_data = base64.b64encode(img_bytes.getvalue()).decode('ascii')
-    with open(file_name, "r") as f:
-        for ligne in f:
-            mots = ligne.split()
-            for mot in mots:
-                print(mot)
-    return render_template('Main.html', img_data=img_data)
-
+    return render_template('Phase3.html')
 if __name__ == '__main__':
     app.run(debug=True)
--- PokerApp2/function.py
+++ PokerApp2/function.py
@@ -1,4 +1,18 @@
-import json
+'''
+Function Name: Generalities
+
+Parameters:
+ - line : first line of one of a file game of a tournament
+        (this file has to be put in the " Game_File" folder)
+
+Returns: 
+ - gene : list of generalitites about the games
+Description:
+    allow to get the date, the tournament id and the hand id
+'''
+from sqlite3 import SQLITE_CREATE_TEMP_INDEX
+
+
 def Generalities(line):
     gene = []
     words = line.split()
@@ -6,130 +20,7 @@ def Generalities(line):
     gene.append(words[4])
     gene.append(words[17:19])
     return gene
-
-
-def raises(index,lines,main_player):
-    Sum = 0
-    i = index
-    words = lines[i].split()
-    while lines[i][:3] !="***" and i > 0:
-        words = lines[i].split()
-        if words[1] == "raises" and words[0] == main_player + ":": 
-            Sum +=int(words[4])
-        elif words[0] == main_player + ":":
-            if words[1] == "brings":
-                Sum-= int(words[4])
-            elif words[1] == "calls":
-                Sum-= int(words[2])
-            elif words[1] == "bets":
-                Sum-= int(words[2])
-        i-=1
-    return Sum
-def Count_Chips(main_player,lines, idSummary):
-    Sum = 0
-    i = 0
-    words = lines[i].split()
-    while i < idSummary:        
-        words = lines[i].split()
-        if words[0] == "Seat" and words[2] == main_player : 
-            Chips = int(words[3][1:])
-        elif words[1] == "posts" and words[0] == main_player + ":":
-            Sum += int(words[4])
-        elif words[0] == main_player + ":":
-            if words[1] == "brings":
-                Sum+= int(words[4])
-            elif words[1] == "calls":
-                Sum+= int(words[2])
-            elif words[1] == "bets":
-                Sum+= int(words[2])
-            elif words[1] == "raises":
-                Sum+= raises(i,lines,main_player)
-            elif words[1] == "folds" or words[1] == "mucks":
-                Sum =-Sum
-                i = idSummary
-        i+=1
-    return Sum
-def Average(lst):
-    return sum(lst) / len(lst)
-def Summary_Chips(game_file,main_player):
-    file_name ='Game_File\\' + game_file
-    lines = []
-    i = 0
-    with open(file_name, "r") as f:
-        for line in f:
-            lines.append(line.strip())
-        list_index = []
-        
-        for line in lines:
-            if line[:5] == '*** 3':
-
-                list_index.append(i)
-            elif line[:5] == '*** 4':
-
-                list_index.append(i)
-            elif line[:5] == '*** 5':
-
-                list_index.append(i)
-            elif line[:5] == '*** 6':
-
-                list_index.append(i)
-            elif line[:5] == '*** R':
-
-                list_index.append(i)
-            elif line[:6] == '*** SH':
-
-                list_index.append(i)
-            elif line[:6] == '*** SU':
-                idSummary = i
-            i+=1     
-    i = idSummary
-    while i < len(lines):
-        words = lines[i].split()
-        if words[2] == main_player and words[3] == "showed":
-            if words[12] == "won" :
-                return int(words[13][1:-1]) - Count_Chips(main_player,lines, idSummary)
-            else:
-                return -Count_Chips(main_player,lines, idSummary)
-        i+=1
-    return Count_Chips(main_player,lines, idSummary)
-def Card_street(word,street_index, lines, main_player):
-    i = 0
-    if word == '3rd':
-        i = street_index[0]
-    elif word == '4th':
-        i = street_index[1]
-    elif word == '5th':
-        i = street_index[2]
-    elif word == '6th':
-        i = street_index[3]
-    elif word == 'River':
-        i = street_index[4]
-    words = lines[i].split()
-    while (words[0] != 'Dealt' or words[2] != main_player):
-        i+=1
-        words = lines[i].split()
-    print("--------------")
-    hand = []
-    for card in words[3:]:
-        c = ''
-        print(card)
-        for letter in card:
-            print(letter)
-            if letter != "[" and  letter != "]":
-                c += letter
-        hand.append(c)
-    return hand
-def Card_to_html(hand):
-    Html_Cards = []
-    num_cards = ['A','2','3','4','5','6','7','8','9','T','J','C','Q','K']
-    shape_card = ['s','h','d','c']
-    for card in hand:
-        i= num_cards.index(card[0])
-        j= shape_card.index(card[1])
-        Html_Cards.append(["#1271" + str(i+16*j + 37)+";",j])
-    return Html_Cards
-
-def Summary_Hands(game_file,main_player):
+def file_index(game_file):
     file_name ='Game_File\\' + game_file
     lines = []
     i = 0
@@ -159,17 +50,164 @@ def Summary_Hands(game_file,main_player):
                 street_index.append(i)
             elif line[:6] == '*** SU':
                 idSummary = i
-            i+=1     
+                street_index.append(i)
+            i+=1
+    return (lines,street_index)
+'''
+Function Name: raises
+
+Parameters:
+ - Sum : get the amount of chips put by on the table when he raised
+
+Returns: 
+ - Sum
+Description:
+    When a player raised the way we calculate the amount of chips put on the table
+    is determined by what the players have done befire
+'''
+def raises(index,lines,main_player):
+    Sum = 0
+    i = index
+    words = lines[i].split()
+    while lines[i][:3] !="***" and i > 0:
+        words = lines[i].split()
+        if words[1] == "raises" and words[0] == main_player + ":": 
+            Sum +=int(words[4])
+        elif words[0] == main_player + ":":
+            if words[1] == "brings":
+                Sum-= int(words[4])
+            elif words[1] == "calls":
+                Sum-= int(words[2])
+            elif words[1] == "bets":
+                Sum-= int(words[2])
+        i-=1
+    return Sum
+
+'''
+Function Name: Count_Chips
+
+Parameters:
+ - Sum
+
+Returns: 
+ - Sum : How much a player put chips
+Description:
+    calculate how much a player put chips during the game 
+'''
+def Count_Chips(main_player,lines, idSummary):
+    Sum = 0
+    i = 0
+    words = lines[i].split()
+    while i < idSummary:        
+        words = lines[i].split()
+        if words[0] == "Seat" and words[2] == main_player : 
+            Chips = int(words[3][1:])
+        elif words[1] == "posts" and words[0] == main_player + ":":
+            Sum += int(words[4])
+        elif words[0] == main_player + ":":
+            if words[1] == "brings":
+                Sum+= int(words[4])
+            elif words[1] == "calls":
+                Sum+= int(words[2])
+            elif words[1] == "bets":
+                Sum+= int(words[2])
+            elif words[1] == "raises":
+                Sum+= raises(i,lines,main_player)
+            elif words[1] == "folds" or words[1] == "mucks":
+                Sum =-Sum
+                i = idSummary
+        i+=1
+    return Sum
+'''
+Function Name: average
+
+Parameters:
+- lst : list of all the chips won or lost played in all game
+
+Returns: 
+ - average of chips won or lost
+Description:
+'''
+def Average(lst):
+    return sum(lst) / len(lst)
+'''
+Function Name: Summary_Chips
+
+Parameters:
+only need the game_file
+
+Returns: 
+ - how much the player have won or have lost 
+'''
+def Summary_Chips(game_file,main_player):
+    (lines,street_index) = file_index(game_file)
+    idSummary = street_index[-1]
     i = idSummary
+    while i < len(lines):
+        words = lines[i].split()
+        if words[2] == main_player and words[3] == "showed":
+            if words[12] == "won" :
+                return int(words[13][1:-1]) - Count_Chips(main_player,lines, idSummary)
+            else:
+                return -Count_Chips(main_player,lines, idSummary)
+        i+=1
+    return Count_Chips(main_player,lines, idSummary)
+'''
+Function Name: Card_Street
+
+
+'''
+def Card_street(word,street_index, lines, player):
+    i = 0
+    if word == '3rd':
+        i = street_index[0]
+    elif word == '4th':
+        i = street_index[1]
+    elif word == '5th':
+        i = street_index[2]
+    elif word == '6th':
+        i = street_index[3]
+    elif word == 'River':
+        i = street_index[4]
+    words = lines[i].split()
+    while (words[0] != 'Dealt' or words[2] != player):
+        i+=1
+        words = lines[i].split()
+    hand = []
+    for card in words[3:]:
+        c = ''
+        for letter in card:
+            if letter != "[" and  letter != "]":
+                c += letter
+        hand.append(c)
+    return hand
+'''
+Function Name: Card_to_html
+
+'''
+def Card_to_html(hand):
+    Html_Cards = []
+    num_cards = ['A','2','3','4','5','6','7','8','9','T','J','C','Q','K']
+    shape_card = ['s','h','d','c']
+    for card in hand:
+        i= num_cards.index(card[0])
+        j= shape_card.index(card[1])
+        Html_Cards.append(["#1271" + str(i+16*j + 37)+";",j])
+    return Html_Cards
+'''
+Function Name: Summary_Hands
+
+'''
+def Summary_Hands(game_file,main_player):
+    (lines,street_index) = file_index(game_file)
+    i = street_index[-1]
     while i < len(lines):
         words = lines[i].split()
         if words[2] == main_player and (words[3] == "showed" or words[3] == "mucked") :
             hand = []
             for card in words[4:11]:
                 c = ''
-                print(card)
                 for letter in card:
-                    print(letter)
                     if letter != "[" and  letter != "]":
                         c += letter
                 hand.append(c)
@@ -177,3 +215,47 @@ def Summary_Hands(game_file,main_player):
         elif words[2] == main_player and words[3] == "folded" :
             return Card_to_html(Card_street(words[6],street_index, lines, main_player))
         i+=1
+
+def Init(game_file):
+    (lines,street_index) = file_index(game_file)
+    Pot = 0
+    i  = 2
+    Players_Init = []
+    while i < street_index[0]:
+        words = lines[i].split()
+        if words[0] =='Seat':
+            Players_Init.append([words[2],int(words[3][1:])])
+        elif words[1] == 'posts':
+            Pot += int(words[4])
+        i+=1
+    return (Players_Init,Pot)
+def Action(lines,line,street):
+    words = line.split()
+    action = [words[0][:-1],words[1]]
+    if words[1] == 'folds' or words[1] == 'checks':
+        return action
+    elif words[1] == 'raises':
+        return action.append(raises(street,lines,words[0][:-1]))
+    elif words[1] == 'calls' or words[1] == 'bets':
+        return action.append(int(words[2]))
+
+def Play(game_file):
+    Players_Actions = []
+    Street_Change = []
+    (lines,street_index) = file_index(game_file)
+    i = street_index[1]
+    street = i 
+    while i < street_index[-1]:
+        if i in street_index:
+            street = i
+        words = lines[i].split()
+        if words[0] == '***':
+            Street_Change.append(i)
+        elif words[0] == 'Dealt':
+            street_words = lines[street].split()
+            Players_Actions.append([words[2],Card_to_html(Card_street(street_words[1],street_index, lines, words[2]))])
+        else:
+            Players_Actions.append(Action(lines,lines[i],street))
+        i+=1
+    print(Players_Actions)
+    return Players_Actions
