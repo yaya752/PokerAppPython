05d1c5a77d2c0a0dd631beacb0be1a9693581a49
Yaya752 <yassine76440@gmail.com>
2023-08-15 17:07:23 +0100

    probs finished

--- PokerApp2/Calcul.py
+++ PokerApp2/Calcul.py
@@ -8,6 +8,6 @@ def Calculate_odds(occur,street,list_numplayers):
     for i in range (len(occur)):
         for j in range(len(occur[1])):
             occur1[i][j] = occur[i][j]
-    low_hand_odds = low_hand_odd(occur1,street,list_numplayers)
-    high_hand_odds = all_prob(occur,street,list_numplayers)
+    #low_hand_odds = low_hand_odd(occur1,street,list_numplayers)
+    (high_hand_odds,low_hand_odds) = all_prob(occur,street,list_numplayers)
     return (occur1,low_hand_odds,high_hand_odds)
